5c74675c2ed4494e88f5377c87946bcc47459daa
Khalil Brown <khalilbrown@khalils-air.myfiosgateway.com>
2021-12-25 01:46:21 -0500

    live

--- packages/solsantas/nft/nft-gen.py
+++ packages/solsantas/nft/nft-gen.py
@@ -2,11 +2,11 @@ import sys
 from shutil import copyfile
 import json
 
-metadata = {"name": "Secret Santas on Sol #0",
+metadata = {"name": "Secret Santas on Solana #0",
             "symbol": "SSoS",
-            "description": "SSoS",
+            "description": "Support Beauty 2 The Streetz by donating 0.033 SOL, receive the SSoS token and exchange it to participate in a Secret Santa NFT exchange!",
             "seller_fee_basis_points": 500,
-            "image": "image.png",
+            "image": "",
             "external_url": "https://solsantas.xyz",
             "attributes": [
                 {
@@ -15,38 +15,37 @@ metadata = {"name": "Secret Santas on Sol #0",
                 }
             ],
             "collection": {
-                "name": "Secret Santas on Sol",
-                "family": "Secret Santas on Sol"
+                "name": "Secret Santas on Solana",
+                "family": "Secret Santas on Solana"
             },
             "properties": {
-                "files": [
-                    {
-                        "uri": "image.png",
-                        "type": "image/png"
-                    }
-                ],
                 "category": "image",
                 "creators": [
                     {
-                        "address": "74oMvuZctGXRcTXptBAjhgzA7VJFjY7w5Fp3CYgysWA3",
+                        "address": "AHbBtMjw4aeEDfUetWLfXV6EDaE5p6VuAv1zgiWNMhiS",
                         "share": 100
                     }
                 ]
             }}
+
+
 def iterate_metadata(iteration):
     new_metadata = metadata
-    new_metadata['name'] = new_metadata['name'].replace(new_metadata['name'],f'Secret Santas on Sol #{str(iteration)}',1)
+    new_metadata['name'] = new_metadata['name'].replace(
+        new_metadata['name'], f'Secret Santas on Solana #{str(iteration +1)}', 1)
+    new_metadata['image'] = f'{iteration}.gif'
     return new_metadata
+
 
 def main(args):
     count = int(args[0])
     for num in range(count):
         updated_metadata = iterate_metadata(num)
-        f = open(f'./assets/{num}.json','w')
+        f = open(f'./assets/{num}.json', 'w')
         f.write(json.dumps(updated_metadata))
         f.close()
-        copyfile('SOL_Santas_WIP_ROUGH.png', f'./assets/{num}.png')
+        copyfile('SSoS_Coin_GIF.gif', f'./assets/{num}.gif')
 
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
