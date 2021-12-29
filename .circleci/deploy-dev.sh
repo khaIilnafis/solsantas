0b14547d6ffa3d47622bcea7bb24cab0af88eb0a
Khalil Brown <khalilbrown@khalils-air.myfiosgateway.com>
2021-12-29 18:34:57 -0500

    fix deploy script

--- .circleci/deploy-dev.sh
+++ .circleci/deploy-dev.sh
@@ -3,9 +3,10 @@ cp ~/.env.development ~/solsantas/packages/solsantas/.env
 cd ~/solsantas/packages/solsantas
 
 git pull origin dev
-
+export PATH=~/.npm-global/bin:$PATH
+npm install -g npm
+source ~/.profile
 npm install
 npm run build
-export PATH=~/.npm-global/bin:$PATH
-source ~/.profile
+
 cp -r build/* /var/www/dev.solsantas.xyz/html/
